d92e455bf548f48d0ee265330dd3f8289c914459
Алекс <chu@mail.ru>
2023-05-23 01:34:04 +0300

    tgbot

--- src/main/java/com/example/tgtestbot/listner/TelegramBotsUpdateListner.java
+++ src/main/java/com/example/tgtestbot/listner/TelegramBotsUpdateListner.java
@@ -1,23 +1,42 @@
 package com.example.tgtestbot.listner;
 
+import com.example.tgtestbot.entity.NotificationTask;
+import com.example.tgtestbot.service.NotificationTaskServise;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Component
 public class TelegramBotsUpdateListner implements UpdatesListener {
     private final TelegramBot telegramBot;
     private final Logger logger = LoggerFactory.getLogger(TelegramBotsUpdateListner.class);
+    private final Pattern pattern = Pattern.compile("(\\d{1,2}\\.\\d{1,2}\\.\\d{4} \\d{1,2}\\:\\d{2})\\s+([А-я\\d\\s,.!?;]+)");
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+    private final NotificationTaskServise notificationTaskServise;
 
-    public TelegramBotsUpdateListner(TelegramBot telegramBot) {
-        this.telegramBot = telegramBot;
+
+
+public TelegramBotsUpdateListner(TelegramBot telegramBot,NotificationTaskServise notificationTaskServise) {
+    this.telegramBot = telegramBot;
+    this.notificationTaskServise = notificationTaskServise;
     }
 
     @PostConstruct
@@ -36,18 +55,51 @@ public class TelegramBotsUpdateListner implements UpdatesListener {
                 String text = message.text();
 
                 if ("/Start".equals(text)) {
-                    SendMessage sendMessage = new SendMessage(chatId, "H1");
-                    SendResponse sendResponse = telegramBot.execute(sendMessage);
-                    if (!sendResponse.isOk()) {
-                        logger.error("Error", sendResponse.description());
+                    sendMessage(chatId,"""
+                    Привет, помогу запланировать задачу, введи дату,
+                    время и задачу в формате 11.12.2023 20:00 сдать бота на проверку""");
 
+                    } else if (text != null) {
+                        Matcher matcher = pattern.matcher(text);
+                    if (matcher.find()) {
+                        LocalDateTime dateTime = parse(matcher.group(1));
+                        if (Objects.isNull(dateTime)) {
+                            sendMessage(chatId, "некорретный формат даты или времени");
+                        } else {
+                            String txt = matcher.group(2);
+                            NotificationTask notificationTask = new NotificationTask();
+                            notificationTask.setChatId(chatId);
+                            notificationTask.setMessage(txt);
+                            notificationTask.setNotificationDateTime(dateTime);
+                            notificationTaskServise.save(notificationTask);
+                            sendMessage(chatId,"успешно сохранено");
+                        }
+
+                    } else {
+                        sendMessage(chatId,"некорректный формат");
                     }
-                }
+                    }
             });
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
 
         }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+@Nullable
+    private LocalDateTime parse(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, dateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    private void sendMessage(Long chatId, String message) {
+        SendMessage sendMessage = new SendMessage(chatId, message);
+        SendResponse sendResponse = telegramBot.execute(sendMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error", sendResponse.description());
+        }
     }
 }
---
+++ src/test/java/com/example/tgtestbot/TgTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgtestbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgTestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
